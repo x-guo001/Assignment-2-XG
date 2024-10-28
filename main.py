d22f6baf6cf1dc2fab78ab7392052169c629345b
Compdrug203 <aicd3compdrug203@gmail.com>
2024-10-28 10:05:32 -0700

    Fixed main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from sklearn.preprocessing import StandardScaler
 def main():
 
     # load data with default settings
-    # You will need to pick the features you want to use! 
-    X_train, X_val, y_train, y_val = utils.loadDataset(features=['gender']
-                                                       , split_percent=0.8, split_state=42)
+    X_train, X_val, y_train, y_val = utils.loadDataset(features=['Penicillin V Potassium 500 MG', 'Computed tomography of chest and abdomen', 
+                                                                  'Plain chest X-ray (procedure)',  'Low Density Lipoprotein Cholesterol',
+                                                                  'Creatinine', 'AGE_DIAGNOSIS'], split_percent=0.8, split_state=42)
 
     # scale data since values vary across features
     sc = StandardScaler()
@@ -22,7 +22,7 @@ def main():
     # for testing purposes once you've added your code
     # CAUTION & HINT: hyperparameters have not been optimized
 
-    log_model = logreg.LogisticRegression(num_feats=1, max_iter=10, tol=0.01, learning_rate=0.00001, batch_size=12)
+    log_model = logreg.LogisticRegression(num_feats=2, max_iter=10, tol=0.01, learning_rate=0.00001, batch_size=12)
     log_model.train_model(X_train, y_train, X_val, y_val)
     log_model.plot_loss_history()
             
